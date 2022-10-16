6e3c35c74ce6df5c68904ea0a398c01749129180
KseniaJarjury <100300372+KseniaJarjury@users.noreply.github.com>
2022-10-16 09:12:05 -0300

    Agregue el FormContratacion
    Tambien algunas imagenes

--- perfilOferente.js
+++ perfilOferente.js
@@ -20,3 +20,19 @@ const contenedorLight = document.querySelector('.imagen-light')
     contenedorLight.classList.toggle('show')
     imagenesLight.classList.toggle('showImage')
  }
+ const exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector('.modal-title')
+  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `New message to ${recipient}`
+  modalBodyInput.value = recipient
+})
